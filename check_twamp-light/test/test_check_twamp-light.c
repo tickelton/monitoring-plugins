8e4adf9eeee4ce97dbc9ac1566cdbd74ba5f0e12
tickelton <tickelton@gmail.com>
2021-06-08 21:48:53 +0200

    Adds more ntp_to_timeval unit tests

--- check_twamp-light/test/test_check_twamp-light.c
+++ check_twamp-light/test/test_check_twamp-light.c
@@ -6,6 +6,8 @@
 #include "check_twamp-light.h"
 #include "unity.h"
 
+#define OFFSET_1900_1970 2208988800
+
 void setUp(void) {}
 
 void tearDown(void) {}
@@ -14,17 +16,45 @@ void test_ntp_to_timeval_tv_should_be_0_at_01_01_1900(void) {
   struct ntp_ts_t ntp_ts;
   struct timeval tv;
 
-  ntp_ts.seconds = 2208988800;
+  ntp_ts.seconds = OFFSET_1900_1970;
   ntp_ts.fraction = 0;
 
   ntp_to_timeval(&ntp_ts, &tv);
 
-  TEST_ASSERT_EQUAL_UINT32(tv.tv_sec, 0);
-  TEST_ASSERT_EQUAL_UINT32(tv.tv_usec, 0);
+  TEST_ASSERT_EQUAL_UINT32(0, tv.tv_sec);
+  TEST_ASSERT_EQUAL_UINT32(0, tv.tv_usec);
+}
+
+void test_ntp_to_timeval_usecs_should_be_500ms(void) {
+  struct ntp_ts_t ntp_ts;
+  struct timeval tv;
+
+  ntp_ts.seconds = OFFSET_1900_1970;
+  ntp_ts.fraction = (1LL << 32) / 2;
+
+  ntp_to_timeval(&ntp_ts, &tv);
+
+  TEST_ASSERT_EQUAL_UINT32(0, tv.tv_sec);
+  TEST_ASSERT_EQUAL_UINT32(500000, tv.tv_usec);
+}
+
+void test_ntp_to_timeval_usecs_should_be_250ms(void) {
+  struct ntp_ts_t ntp_ts;
+  struct timeval tv;
+
+  ntp_ts.seconds = OFFSET_1900_1970;
+  ntp_ts.fraction = (1LL << 32) / 4;
+
+  ntp_to_timeval(&ntp_ts, &tv);
+
+  TEST_ASSERT_EQUAL_UINT32(0, tv.tv_sec);
+  TEST_ASSERT_EQUAL_UINT32(250000, tv.tv_usec);
 }
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_ntp_to_timeval_tv_should_be_0_at_01_01_1900);
+  RUN_TEST(test_ntp_to_timeval_usecs_should_be_500ms);
+  RUN_TEST(test_ntp_to_timeval_usecs_should_be_250ms);
   return UNITY_END();
 }

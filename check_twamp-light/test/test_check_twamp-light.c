3968e6bb7c9b021a92d9ebd66e52cd986ad3bce1
tickelton <tickelton@gmail.com>
2021-06-08 21:32:29 +0200

    Adds code formatting make target

--- check_twamp-light/test/test_check_twamp-light.c
+++ check_twamp-light/test/test_check_twamp-light.c
@@ -3,35 +3,28 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include "unity.h"
 #include "check_twamp-light.h"
+#include "unity.h"
 
-void setUp(void)
-{
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_ntp_to_timeval_tv_should_be_0_at_01_01_1900(void) {
+  struct ntp_ts_t ntp_ts;
+  struct timeval tv;
+
+  ntp_ts.seconds = 2208988800;
+  ntp_ts.fraction = 0;
+
+  ntp_to_timeval(&ntp_ts, &tv);
+
+  TEST_ASSERT_EQUAL_UINT32(tv.tv_sec, 0);
+  TEST_ASSERT_EQUAL_UINT32(tv.tv_usec, 0);
 }
 
-void tearDown(void)
-{
-}
-
-void test_ntp_to_timeval_tv_should_be_0_at_01_01_1900(void)
-{
-    struct ntp_ts_t ntp_ts;
-    struct timeval tv;
-
-    ntp_ts.seconds = 2208988800;
-    ntp_ts.fraction = 0;
-
-    ntp_to_timeval(&ntp_ts, &tv);
-
-    TEST_ASSERT_EQUAL_UINT32(tv.tv_sec, 0);
-    TEST_ASSERT_EQUAL_UINT32(tv.tv_usec, 0);
-}
-
-
-int main(void)
-{
-    UNITY_BEGIN();
-    RUN_TEST(test_ntp_to_timeval_tv_should_be_0_at_01_01_1900);
-    return UNITY_END();
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_ntp_to_timeval_tv_should_be_0_at_01_01_1900);
+  return UNITY_END();
 }
